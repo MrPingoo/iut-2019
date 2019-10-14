f6979076a78d45373c81ee3d79065968b8233d85
Julian Layen <layenjulian@gmail.com>
2019-10-14 14:04:04 +0200

    code

---
+++ code/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig', [
+            'vars' => ['Hello World', 'Hello IUT']
+        ]);
+    }
+}
+
